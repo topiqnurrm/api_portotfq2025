5111fcba1685ec1685484e21672dd0f9d2eecbad
Taufiq Nurrohman <sossmeed@gmail.com>
2025-11-10 21:32:48 +0700

    page 2 kegiatan presma done

--- app/galeri/kegiatan/page.jsx
+++ app/galeri/kegiatan/page.jsx
@@ -17,8 +17,6 @@ export default function KegiatanGallery() {
   // Mapping untuk jenis
   const jenisMap = {
     LOM: "Lomba",
-    ABDI: "Pengabdian Masyarakat",
-    CAR: "Karier",
     SEM: "Seminar",
     PEL: "Pelatihan",
     ORG: "Organisasi",
@@ -259,7 +257,7 @@ export default function KegiatanGallery() {
                       <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                       </svg>
-                      Download Dokumen
+                      Lihat Sertifikat
                     </a>
                   </div>
                 )}
